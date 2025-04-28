a440d6e9c3b1d4dc5250525e90e757e30f9afe36
ekastn <118880703+ekastn@users.noreply.github.com>
2025-04-28 16:05:20 +0700

    Pertemuan 06

---
+++ Pertemuan06/Praktikum/backend/config/database.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var (
+	DBName                     = "db2025"
+	MahasiswaCollection        = "data_mahasiswa"
+	MongoString         string = os.Getenv("MONGOSTRING")
+)
+
+func MongoConnect(dbname string) (db *mongo.Database) {
+	MongoString = os.Getenv("MONGOSTRING")
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(MongoString))
+	if err != nil {
+		fmt.Printf("MongoConnect: %v\n", err)
+	}
+	return client.Database(dbname)
+}
